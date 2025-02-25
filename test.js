571b2e4311980167fc8347bf47d81165e05df2d5
Peter Kwong <peterpoonkwong@gmail.com>
2025-02-24 22:27:02 -0600

    Update test.js

--- test.js
+++ test.js
@@ -5,16 +5,16 @@ describe('GET /', () => {
     let server;
 
     beforeAll((done) => {
-        server = app.listen(4000, () => done()); // Start the server before tests
+        server = app.listen(4000, () => done()); // Start server on a test-specific port
     });
 
     afterAll((done) => {
-        server.close(() => done()); // Close the server after tests
+        server.close(() => done()); // Properly close the server
     });
 
     it('should return Hello World', async () => {
         const res = await request(server).get('/');
         expect(res.text).toBe('Hello World');
         expect(res.statusCode).toBe(200);
-    }, 10000); // Set a timeout of 10 seconds
+    });
 });
