33b19eae7b42b6d5e492bdb0a6dffa231c0d7a74
Peter Kwong <peterpoonkwong@gmail.com>
2025-02-24 22:18:48 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,19 @@ const request = require('supertest');
 const app = require('./http-server');
 
 describe('GET /', () => {
+    let server;
+
+    beforeAll((done) => {
+        server = app.listen(4000, () => done()); // Start the server before tests
+    });
+
+    afterAll((done) => {
+        server.close(() => done()); // Close the server after tests
+    });
+
     it('should return Hello World', async () => {
-        const res = await request(app).get('/');
+        const res = await request(server).get('/');
         expect(res.text).toBe('Hello World');
         expect(res.statusCode).toBe(200);
-    });
+    }, 10000); // Set a timeout of 10 seconds
 });
